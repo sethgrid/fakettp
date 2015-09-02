f4cf554193f90af285b397032756aaa288252082
Seth Ammons <seth@sendgrid.com>
2015-09-02 13:40:35 -0700

    updated some formatting and are now tracking requests with an id

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"strings"
 	"time"
@@ -23,18 +24,22 @@ var ProxyPort int
 // StringSlice adheres to the flag Var interface, and allows for the -header flag to be reused
 type StringSlice []string
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func main() {
 	flag.IntVar(&Port, "port", 5000, "set the port on which to listen")
 	flag.IntVar(&ResponseCode, "code", 200, "set the http status code with which to respond")
 	flag.DurationVar(&ResponseTime, "response_time", time.Millisecond*10, "set the response time, ex: 250ms or 1m5s")
 	flag.StringVar(&ResponseBody, "body", "", "set the response body")
-	flag.StringVar(&HyjackRoute, "hyjack", "/", "set the route you wish to hijack if using the reverse proxy host and port")
+	flag.StringVar(&HyjackRoute, "hyjack", "", "set the route you wish to hijack if using the reverse proxy host and port")
 	flag.StringVar(&ProxyHost, "proxy_host", "http://0.0.0.0", "the host we will reverse proxy to (include protocol)")
 	flag.IntVar(&ProxyPort, "proxy_port", 0, "the proxy port")
-	flag.Var(&ResponseHeaders, "header", "headers, ex: 'Content-Type: application/json'")
+	flag.Var(&ResponseHeaders, "header", "headers, ex: 'Content-Type: application/json'. Multiple -header parameters allowed.")
 	flag.Parse()
 
-	log.Printf("starting on port :%d with response code %d and response time %s with headers %v", Port, ResponseCode, ResponseTime.String(), ResponseHeaders)
+	log.Printf("starting on port :%d", Port)
 
 	http.HandleFunc("/", defaultHandler)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", Port), nil)
@@ -45,10 +50,15 @@ func main() {
 
 // defaultHanlder will either proxy the request or substitute in the hyjack data
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("new request, waiting %s", ResponseTime.String())
+	// capture a request id with padding and leading zeros incase multiple requests
+	// come in at the same time
+	reqID := fmt.Sprintf("[%07x] ", rand.Int31n(1e8))
+	log.SetPrefix(reqID)
+
+	log.Printf("new request")
 
 	if HyjackRoute == "" || HyjackRoute == r.URL.String() {
-		log.Printf("hyjacking route %s", HyjackRoute)
+		log.Printf("hyjacking route %s (%s)", HyjackRoute, ResponseTime.String())
 		<-time.Tick(ResponseTime)
 
 		for _, header := range ResponseHeaders {
@@ -69,7 +79,7 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("proxying request")
 
-	req, err := http.NewRequest(r.Method, fmt.Sprintf("%s:%d", ProxyHost, ProxyPort), r.Body)
+	req, err := http.NewRequest(r.Method, fmt.Sprintf("%s:%d%s", ProxyHost, ProxyPort, r.RequestURI), r.Body)
 	if err != nil {
 		log.Printf("error with proxy request - %v", err)
 		return
