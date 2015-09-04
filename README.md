a8684d0c8df5fca312bb0fcb4f533137fbca0ece
Seth Ammons <seth@sendgrid.com>
2015-09-03 18:08:16 -0700

    added ability to use a config file

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Fakettp is an http debugging proxy allowing you to easily set up responses for h
   - response headers
   - response body
 
-You can have the above as global responses, or you can pass in an individual endpoint where those responses apply, and proxy all other endpoints to the original service. This gives you the ability to test your system against the other service and simulate an error on a single endpoint while the rest of the service works normally.
+You can set global responses, or you can pass in individual endpoints where those responses apply, and proxy all other endpoint requests to the original service. This gives you the ability to test your system against the other service and simulate an error on a single endpoint while the rest of the service works normally.
 
 Use Case
 --------
 
-As a developer, you want to test how your application behaves when another service's endpoint misbehaves. Have your application send the requests to fakettp, and have fakettp proxy requests to the original service or return specific responses.
+What inspired this repo!: We had Service A calling Service B's endpoints internally. We needed some of the endpoints to do their job, while for one of them we needed to verify how Service A behaved when it returned different content and status codes. After using this tool, our QA was able to verify all the edge-cases they were interested in!
 
 Sample Usage
 ------------
@@ -23,6 +23,11 @@ From the source or compiled binary, just set the response you want (note that yo
 Hyjac a single endpoint and proxy all other calls:
 ```
 go run main.go -proxy_host http://example.com -proxy_port 9092 -hyjack /api/user -port 5555 -code 418 -body "I'm a teapot"
+```
+
+We can also use a config file instead (more on this below!):
+```
+go run main.go -config my.conf
 ```
 
 Return the same data for all calls to this service:
@@ -51,6 +56,40 @@ $ curl -v localhost:5555/foo/bar/raz -d '{"foo":"bar"}'
 <
 * Connection #0 to host localhost left intact
 {"json":true}
+```
+
+Config File
+-----------
+
+When passing command line flags, you are limited to either hyjacking all requests or only requests to a single endpoint. With a config file, you can specify multiple routes to behave differently.
+
+Sample Config:
+```json
+{
+    "proxy_host": "0.0.0.0",
+    "proxy_port": 9092,
+    "port": 5000,
+    "fakes": [
+        {
+            "hyjack": "/api/settings.json",
+            "code": 500
+        },
+        {
+            "hyjack": "/api/users.json",
+            "methods": [
+                "GET",
+                "POST"
+            ],
+            "body": "{\"json\":true}",
+            "code": 201,
+            "headers": [
+                "Content-Type: application/json",
+                "Cache-Control: max-age=3600"
+            ],
+            "time": "1m3s15ms"
+        }
+    ]
+}
 ```
 
 Sample Logs
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -11,44 +12,113 @@ import (
 	"time"
 )
 
-var Port int
-var ResponseCode int
-var ResponseTime time.Duration
-var ResponseBody string
-var ResponseHeaders StringSlice
-
-var Methods StringSlice
-var HyjackRoute string
-var ProxyHost string
-var ProxyPort int
-
 // StringSlice adheres to the flag Var interface, and allows for the -header flag to be reused
 type StringSlice []string
+
+type Config struct {
+	ProxyHost string `json:"proxy_host"`
+	ProxyPort int    `json:"proxy_port"`
+	Port      int    `json:"port"`
+	Fakes     []Fake `json:"fakes"`
+}
+
+type Fake struct {
+	HyjackPath      string      `json:"hyjack"`
+	Methods         StringSlice `json:"methods"`
+	ResponseBody    string      `json:"body"`
+	ResponseCode    int         `json:"code"`
+	ResponseHeaders StringSlice `json:"headers"`
+	ResponseTimeRaw string      `json:"time"`
+	ResponseTime    time.Duration
+}
+
+var GlobalConfig *Config
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
+	var ConfigPath string
+
+	var Port int
+	var ResponseCode int
+	var ResponseTime time.Duration
+	var ResponseBody string
+	var ResponseHeaders StringSlice
+
+	var Methods StringSlice
+	var HyjackPath string
+	var ProxyHost string
+	var ProxyPort int
+
+	flag.StringVar(&ConfigPath, "config", "", "json formatted conf file (see README at github.com/sethgrid/fakettp). If this flag is used, no other flags will be recognized.")
+
 	flag.IntVar(&Port, "port", 5000, "set the port on which to listen")
 	flag.IntVar(&ResponseCode, "code", 200, "set the http status code with which to respond")
-	flag.DurationVar(&ResponseTime, "response_time", time.Millisecond*10, "set the response time, ex: 250ms or 1m5s")
+	flag.DurationVar(&ResponseTime, "time", time.Millisecond*10, "set the response time, ex: 250ms or 1m5s")
 	flag.StringVar(&ResponseBody, "body", "", "set the response body")
 	flag.Var(&ResponseHeaders, "header", "headers, ex: 'Content-Type: application/json'. Multiple -header parameters allowed.")
 
 	flag.Var(&Methods, "method", "used with the -hyjack route to limit hyjacking to the given http verb. Multiple -method parameters allowed.")
-	flag.StringVar(&HyjackRoute, "hyjack", "", "set the route you wish to hijack if using the reverse proxy host and port")
+	flag.StringVar(&HyjackPath, "hyjack", "", "set the route you wish to hijack if using the reverse proxy host and port")
 	flag.StringVar(&ProxyHost, "proxy_host", "http://0.0.0.0", "the host we will reverse proxy to (include protocol)")
 	flag.IntVar(&ProxyPort, "proxy_port", 0, "the proxy port")
 	flag.Parse()
 
-	log.Printf("starting on port :%d", Port)
+	if ConfigPath != "" {
+		// if we have a config file, use it for all config values
+		data, err := ioutil.ReadFile(ConfigPath)
+		if err != nil {
+			log.Fatalf("%s", string(data))
+		}
+		config := &Config{}
+		err = json.Unmarshal(data, &config)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+
+		GlobalConfig = config
+
+		// set all the response times from config file string to time.Duration
+		for _, fake := range GlobalConfig.Fakes {
+			d, err := time.ParseDuration(fake.ResponseTimeRaw)
+			if err != nil {
+				log.Fatalf("%v", err)
+			}
+			fake.ResponseTime = d
+		}
+
+		log.Printf("starting on port :%d using config %s", config.Port, ConfigPath)
+
+	} else {
+		// if we did not get a config generated from a file, generate one from the
+		// passed in flags
+		config := &Config{}
+		config.Port = Port
+		config.ProxyHost = ProxyHost
+		config.ProxyPort = ProxyPort
+
+		fake := &Fake{}
+		fake.ResponseHeaders = ResponseHeaders
+		fake.HyjackPath = HyjackPath
+		fake.ResponseBody = ResponseBody
+		fake.ResponseCode = ResponseCode
+		fake.ResponseTime = ResponseTime
+
+		config.Fakes = []Fake{*fake}
+
+		GlobalConfig = config
+
+		log.Printf("starting on port :%d", config.Port)
+	}
 
 	http.HandleFunc("/", defaultHandler)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", Port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", GlobalConfig.Port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
 
 // defaultHanlder will either proxy the request or substitute in the hyjack data
@@ -60,30 +130,31 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("new request %s %s", r.Method, r.RequestURI)
 
-	if willHyjack(r.Method, Methods, r.URL.Path, HyjackRoute) {
-		log.Printf("hyjacking route %s (waiting %s)", HyjackRoute, ResponseTime.String())
-		<-time.Tick(ResponseTime)
+	for _, fake := range GlobalConfig.Fakes {
+		if willHyjack(r.Method, fake.Methods, r.URL.Path, fake.HyjackPath) {
+			log.Printf("hyjacking route %s (waiting %s)", fake.HyjackPath, fake.ResponseTime.String())
+			<-time.Tick(fake.ResponseTime)
 
-		for _, header := range ResponseHeaders {
-			parts := strings.Split(header, ": ")
+			for _, header := range fake.ResponseHeaders {
+				parts := strings.Split(header, ": ")
 
-			if len(parts) == 2 && parts[0] != "" && parts[1] != "" {
-				key, value := parts[0], parts[1]
-				log.Printf("setting header %s:%s", key, value)
-				w.Header().Add(key, value)
-			} else {
-				log.Printf("skipping header %s (need a value on both sides of :)", header)
+				if len(parts) == 2 && parts[0] != "" && parts[1] != "" {
+					key, value := parts[0], parts[1]
+					log.Printf("setting header %s:%s", key, value)
+					w.Header().Add(key, value)
+				} else {
+					log.Printf("skipping header %s (need a value on both sides of :)", header)
+				}
 			}
+			w.WriteHeader(fake.ResponseCode)
+			w.Write([]byte(fake.ResponseBody))
+			return
 		}
-		w.WriteHeader(ResponseCode)
-		w.Write([]byte(ResponseBody))
-		return
 	}
-
 	// not hyjacking this time
 	log.Println("proxying request")
 
-	req, err := http.NewRequest(r.Method, fmt.Sprintf("%s:%d%s", ProxyHost, ProxyPort, r.RequestURI), r.Body)
+	req, err := http.NewRequest(r.Method, fmt.Sprintf("%s:%d%s", GlobalConfig.ProxyHost, GlobalConfig.ProxyPort, r.RequestURI), r.Body)
 	if err != nil {
 		log.Printf("error with proxy request - %v", err)
 		return
